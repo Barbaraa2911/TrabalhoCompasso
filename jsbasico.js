a3c6a7393d025caf249352e51ca006d094122a45
Barbara <Barbaragranville666@gmail.com>
2019-07-08 17:11:07 -0300

    primeira tela

---
+++ jsbasico.js
@@ -0,0 +1,6 @@
+var capturando = document.getElementById("primeira-pergunta").value:
+
+function capturar() {
+    document.getElementById("valorDigitado").innerHTML = capturando;
+}
+console.log(capturando)
