c83b40459d73fca6bc621982b7b49fb4d4e4dc32
Barbara <Barbaragranville666@gmail.com>
2019-07-10 14:05:48 -0300

    organizãço dos arquivos

--- jsbasico.js
+++ jsbasico.js
@@ -1,6 +1,35 @@
-var capturando = document.getElementById("primeira-pergunta").value:
+function estado()
+            {
+                var type = document.getElementsByName("estado-emocional");
+                if(type[0].checked)
+                {
+                    alert("bem");
+                }
+                else if(type[1].checked)
+                {
+                    alert("medio");
+                }
+                else if(type[2].checked)
+                {
+                    alert("mal");
+                }
+            }
 
-function capturar() {
-    document.getElementById("valorDigitado").innerHTML = capturando;
-}
-console.log(capturando)
+function func()
+            {
+                var type = document.getElementsByName("type");
+                if(type[0].checked)
+                {
+                    alert("Human");
+                }
+                else if(type[1].checked)
+                {
+                    alert("Robot");
+                }
+                else if(type[2].checked)
+                {
+                    alert("Animals");
+                }
+            }
+
+
